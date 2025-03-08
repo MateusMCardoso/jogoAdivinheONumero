d58505f24be5cad7e1567f750a112edca0c15532
Desktop <mateus.mendonca.cardoso@gmail.com>
2025-03-08 18:42:21 -0300

    Projeto inicial

---
+++ app.js
@@ -0,0 +1,60 @@
+let listaDeNumerosSorteados = [];
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    }
+
+function exibirMensagemInicial(){
+    exibirTextoNaTela('h1','Jogo do número secreto!');
+    exibirTextoNaTela('p', 'Adivinhe o número de 1 a 100:');
+}
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1','Você acertou!');
+        let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagem = `Você acertou o número com ${tentativas} ${palavraTentativas}! Parabéns`;
+        exibirTextoNaTela('p', mensagem);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        
+    }  else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'Número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'Número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }  
+}
+
+function gerarNumeroAleatorio(){
+   let numeroEscolhido = parseInt(Math.random()*100+1);
+   let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+   if (quantidadeDeElementosNaLista == 100){
+    listaDeNumerosSorteados = [];
+  }
+   if (listaDeNumerosSorteados.includes(numeroEscolhido)){
+    return gerarNumeroAleatorio();
+   } else {
+    listaDeNumerosSorteados.push(numeroEscolhido);
+    return numeroEscolhido;
+   }
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = null;
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('diseblad', true);
+}
